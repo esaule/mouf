dc90e4098829ea7ef1df482513ea5c9dcb4bbc04
Erik Saule <esaule@uncc.edu>
2020-03-21 11:45:27 -0400

    removing arm optimizatio to enable compilation in 64 bit

--- mouf/jni/pixman.mk
+++ mouf/jni/pixman.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-LIBPIXMAN_SRC= \
+#LIBPIXMAN_SRC= \
 	       pixman/pixman/pixman-access.c                \
 	       pixman/pixman/pixman-access-accessors.c      \
 	       pixman/pixman/pixman-cpu.c                   \
@@ -31,9 +31,36 @@ LIBPIXMAN_SRC= \
 	       pixman-extra/pixman-combine32.c              \
 	       pixman-extra/pixman-combine64.c              \
 
+LIBPIXMAN_SRC= \
+	       pixman/pixman/pixman-access.c                \
+	       pixman/pixman/pixman-access-accessors.c      \
+	       pixman/pixman/pixman-cpu.c                   \
+	       pixman/pixman/pixman-gradient-walker.c       \
+	       pixman/pixman/pixman-region16.c              \
+	       pixman/pixman/pixman-region32.c              \
+	       pixman/pixman/pixman-image.c                 \
+	       pixman/pixman/pixman-implementation.c        \
+	       pixman/pixman/pixman-general.c               \
+	       pixman/pixman/pixman.c                       \
+	       pixman/pixman/pixman-fast-path.c             \
+	       pixman/pixman/pixman-solid-fill.c            \
+	       pixman/pixman/pixman-conical-gradient.c      \
+	       pixman/pixman/pixman-linear-gradient.c       \
+	       pixman/pixman/pixman-radial-gradient.c       \
+	       pixman/pixman/pixman-bits-image.c            \
+	       pixman/pixman/pixman-utils.c                 \
+	       pixman/pixman/pixman-edge.c                  \
+	       pixman/pixman/pixman-edge-accessors.c        \
+	       pixman/pixman/pixman-trap.c                  \
+	       pixman/pixman/pixman-timer.c                 \
+	       pixman/pixman/pixman-matrix.c                \
+	       pixman-extra/pixman-combine32.c              \
+	       pixman-extra/pixman-combine64.c              \
 
 
-LIBPIXMAN_CFLAGS:=-D_USE_MATH_DEFINES -DPIXMAN_NO_TLS -DPACKAGE="android-cairo" -DUSE_ARM_NEON -DUSE_ARM_SIMD
+
+#LIBPIXMAN_CFLAGS:=-D_USE_MATH_DEFINES -DPIXMAN_NO_TLS -DPACKAGE="android-cairo" # -DUSE_ARM_NEON -DUSE_ARM_SIMD
+LIBPIXMAN_CFLAGS:=-D_USE_MATH_DEFINES -DPIXMAN_NO_TLS -DPACKAGE="android-cairo" 
 
 include $(CLEAR_VARS)
 
